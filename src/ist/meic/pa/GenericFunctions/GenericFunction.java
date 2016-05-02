6f454b19db70c267a62d32fd759e6a0863520c1a
Rafael-Barreira <rafael.spam.barreira@gmail.com>
2016-05-02 14:53:11 +0100

    code \o/

--- src/ist/meic/pa/GenericFunctions/GenericFunction.java
+++ src/ist/meic/pa/GenericFunctions/GenericFunction.java
@@ -1,5 +1,13 @@
 package ist.meic.pa.GenericFunctions;
 
-public class GenericFunction {
+import java.util.List;
+import java.util.TreeMap;
 
+public class GenericFunction {
+	
+	TreeMap<String, List<GFMethod>> functionMethods; 
+	
+	GenericFunction (String functionName){
+		functionMethods.put(functionName, null);
+	}
 }
