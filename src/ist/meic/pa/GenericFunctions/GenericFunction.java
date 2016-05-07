79e538ece7ce421c51ed640e8a124671f9b9cbbe
Rafael-Barreira <rafael.spam.barreira@gmail.com>
2016-05-07 18:49:52 +0100

    update

--- src/ist/meic/pa/GenericFunctions/GenericFunction.java
+++ src/ist/meic/pa/GenericFunctions/GenericFunction.java
@@ -1,6 +1,12 @@
 package ist.meic.pa.GenericFunctions;
 
+import g14.src.ist.meic.pa.ClassPool;
+
 import java.util.List;
+
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.ClassPool;
 
 public class GenericFunction {
 	
@@ -28,6 +34,15 @@ public class GenericFunction {
 	}
 	
 	Object call (Object...objects ){
+		List<GFMethod> applicableMethods = null;
+		
+		ClassPool pool = ClassPool.getDefault();
+		CtClass cc = pool.getCtClass("GFMethod");
+		
+		// get declared methods from every instance of GFMethod in every list
+		// compare arguments 
+		// method combination
+		// profit
 		
 		return objects;
 	}
